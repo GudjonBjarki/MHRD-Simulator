cbeca212029731939f169e4c9a8e87132dd6a8fc
Gudjon <gudjonbjarkia@gmail.com>
2023-12-04 22:25:51 +0000

    Update build schematic function

--- python-demo/schematic.py
+++ python-demo/schematic.py
@@ -48,12 +48,61 @@ nand_schematic = Schematic(
 )
 
 
+def build_schematic_id(schematic_id: str) -> SchematicId:
+    return SchematicId(schematic_id)
+
+
+def build_input_pins(pins_names: list[str]) -> set[PinId]:
+    return set([PinId(pin) for pin in pins_names])
+
+
+def build_output_pins(pins_names: list[str]) -> set[PinId]:
+    return set([PinId(pin) for pin in pins_names])
+
+
+def build_schematic_components(
+    components: list[tuple[str, str]]
+) -> dict[SchematicComponentId, SchematicId]:
+    return {SchematicComponentId(name): SchematicId(type) for name, type in components}
+
+
+def build_connections(
+    connection_tuples: list[tuple[tuple[str, str], tuple[str, str]]]
+) -> set[Connection]:
+    connections: list[Connection] = []
+    for (source_name, source_pin), (
+        destination_name,
+        destination_pin,
+    ) in connection_tuples:
+        if source_name == "input":
+            source_component = SchematicInput()
+
+        else:
+            source_component = SchematicComponentId(source_name)
+
+        if destination_name == "output":
+            destination_component = SchematicOutput()
+
+        else:
+            destination_component = SchematicComponentId(destination_name)
+
+        source = OutputAttachmentPoint(source_component, OutputPinId(source_pin))
+
+        destination = InputAttachmentPoint(
+            destination_component, InputPinId(destination_pin)
+        )
+
+        connections.append(Connection(source, destination))
+
+    return set(connections)
+
+
 def build_schematic(
-    schematic_id: SchematicId,
-    input_pins: set[PinId],
-    output_pins: set[PinId],
-    components: dict[SchematicComponentId, SchematicId],
-    connections: set[Connection],
+    schematic_id: str,
+    input_pins: list[str],
+    output_pins: list[str],
+    components: list[tuple[str, str]],
+    connections: list[tuple[tuple[str, str], tuple[str, str]]],
 ):
     """
     Construct a new schematic.
@@ -66,12 +115,18 @@ def build_schematic(
         Check for circular connections.
     """
 
+    schematic_id = build_schematic_id(schematic_id)
+    schematic_input_pins = build_input_pins(input_pins)
+    schematic_output_pins = build_output_pins(output_pins)
+    schematic_components = build_schematic_components(components)
+    schematic_connections = build_connections(connections)
+
     schematic = Schematic(
         schematic_id=schematic_id,
-        input_pins=input_pins,
-        output_pins=output_pins,
-        components=components,
-        connections=connections,
+        input_pins=schematic_input_pins,
+        output_pins=schematic_output_pins,
+        components=schematic_components,
+        connections=schematic_connections,
     )
 
     return schematic
